4ffe13969468d2a918ec8ab33631d79087b0b009
Jayson Nabor <sonabstudios@gmail.com>
2018-03-25 14:08:25 +0800

    added dynamodb resource

---
+++ backend/serverless/aws-dynamodb-crud/GetItem.js
@@ -0,0 +1,19 @@
+const AWS = require('aws-sdk')
+var dynamodb = new AWS.DynamoDB({region: 'ap-southeast-1', apiVersion: '2012-08-10'})
+
+exports.handler = (event, context, callback) => {
+    const params = {
+        Key: event.Key,
+        ReturnConsumedCapacity: event.ReturnConsumedCapacity,
+        TableName: event.TableName
+    }        
+    dynamodb.getItem(params, function(err, data) {
+        if (err) {
+            console.log(err, err.stack)
+            callback(err, err.stack)
+        } else {
+            console.log(data)
+            callback(null, data)
+        }    
+    })
+}
---
+++ backend/serverless/aws-dynamodb-crud/ScanItems.js
@@ -0,0 +1,18 @@
+const AWS = require('aws-sdk')
+var dynamodb = new AWS.DynamoDB({region: 'ap-southeast-1', apiVersion: '2012-08-10'})
+
+exports.handler = (event, context, callback) => {
+    const params = {
+        ReturnConsumedCapacity: event.ReturnConsumedCapacity,
+        TableName: event.TableName
+    }    
+    dynamodb.scan(params, function(err, data) {
+        if (err) {
+            console.log(err, err.stack)
+            callback(err, err.stack)
+        } else {
+            console.log(data)
+            callback(null, data)
+        }    
+    })
+}
